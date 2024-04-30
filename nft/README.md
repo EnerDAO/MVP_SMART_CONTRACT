4d9ef0542b2304578f43d83ad9b9d3f16e6a4851
Dmytro Biriukov <dimbirua@gmail.com>
2024-04-30 14:37:19 +0300

    nft contract added

---
+++ nft/README.md
@@ -0,0 +1,15 @@
+# NFT on Soroban
+
+## Overview
+
+The "NFT on Soroban" repository showcases a proof of concept (POC) implementation of a non-fungible token (NFT) on the Soroban blockchain platform, adhering to the ERC721 standard and compatible with Preview 11.
+
+## Key Features
+
+The smart contract, named `NonFungibleToken`, offers key functionalities that align with the ERC721 standard. These features include the initialization of the NFT with an administrator, retrieving the NFT's name and symbol, fetching the token URI, checking the total supply, minting new tokens, retrieving the owner of a specific token, and facilitating token transfers between addresses.
+
+## Objectives and Goals of Creating this Smart Contract
+
+The primary goal of this smart contract is to provide a foundational implementation of non-fungible tokens on the Soroban blockchain. It adheres to the widely accepted ERC721 standard, ensuring compatibility with existing ecosystems and platforms. The contract aims to establish a decentralized and secure method for creating, managing, and transferring unique digital assets within the Soroban blockchain. Key objectives include initializing the NFT with an administrator, allowing seamless minting of new tokens, enabling ownership tracking, and facilitating secure transfers between users.
+
+In summary, the "NFT on Soroban" smart contract serves as a fundamental building block for non-fungible tokens on the Soroban blockchain, providing a secure and standardized solution for token creation, ownership management, and transfers.
---
+++ nft/src/test.rs
@@ -0,0 +1,78 @@
+// 1. ERC721 function currently available for NFT: mint, transfer, owner_of
+#![cfg(test)]
+
+extern crate std;
+
+use crate::{contract::NonFungibleToken, NonFungibleTokenClient};
+use soroban_sdk::{
+    testutils::{Address as _, Logs},
+    Address, Env, IntoVal, String
+};
+
+fn create_token<'a>(env: &Env, admin: &Address) -> NonFungibleTokenClient<'a> {
+    let token = NonFungibleTokenClient::new(env, &env.register_contract(None, NonFungibleToken {}));
+    token.initialize(admin, &"EnerDAO NFT".into_val(env), &"EnerDAO".into_val(env));
+    token
+}
+
+#[test]
+fn test_mint() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let user1 = Address::generate(&env);
+    let user2 = Address::generate(&env);
+    let nft = create_token(&env, &admin);
+
+    nft.mint(&user1, &1, &String::from_str(&env, "https://music.youtube.com/watch?v=yRVotpLaCD4"));
+    nft.mint(&user2, &2, &String::from_str(&env, "https://music.youtube.com/watch?v=yRVotpLaCD4"));
+    nft.mint(&user1, &3, &String::from_str(&env, "https://music.youtube.com/watch?v=yRVotpLaCD4"));
+
+    std::println!("{}", env.logs().all().join("\n"));
+}
+
+#[test]
+fn test_owner_of() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let user1 = Address::generate(&env);
+    let nft = create_token(&env, &admin);
+
+    nft.mint(&user1, &1, &String::from_str(&env, "https://music.youtube.com/watch?v=yRVotpLaCD4"));
+
+    assert_eq!(nft.owner_of(&1), user1);
+    std::println!("{}", env.logs().all().join("\n"));
+}
+
+#[test]
+fn test_transfer() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let user1 = Address::generate(&env);
+    let user2 = Address::generate(&env);
+    let nft = create_token(&env, &admin);
+    nft.mint(&user1, &1, &String::from_str(&env, "https://music.youtube.com/watch?v=yRVotpLaCD4"));
+    assert_eq!(nft.owner_of(&1), user1);
+    nft.transfer(&user1, &user2, &1);
+    assert_eq!(nft.owner_of(&1), user2);
+    nft.transfer(&user2, &user1, &1);
+    assert_eq!(nft.owner_of(&1), user1);
+    std::println!("{}", env.logs().all().join("\n"));
+}
+
+#[test]
+#[should_panic(expected = "ID already minted")]
+fn seat_already_taken() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let user1 = Address::generate(&env);
+    let user2 = Address::generate(&env);
+    let nft = create_token(&env, &admin);
+    nft.mint(&user1, &1, &String::from_str(&env, "https://music.youtube.com/watch?v=yRVotpLaCD4"));
+
+    nft.mint(&user2, &1, &String::from_str(&env, "https://music.youtube.com/watch?v=yRVotpLaCD4"));
+    std::println!("{}", env.logs().all().join("\n"));
+}
